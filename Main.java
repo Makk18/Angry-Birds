4ceb3cc28ca72c20b3619bfaee94b2d8f67fdd85
Makk18 <147920891+Makk18@users.noreply.github.com>
2024-10-25 10:58:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,22 @@
 package com.BirdsAngry;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-    public SpriteBatch batch;
+//    public SpriteBatch batch;
+    private Music backgroudnmusic;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
+        backgroudnmusic = Gdx.audio.newMusic(Gdx.files.internal("backgroundmusic.mp3"));
+        backgroudnmusic.setLooping(true);
+        backgroudnmusic.play();
+//        batch = new SpriteBatch();
         this.setScreen(new LoadingScreen(this));
     }
 
@@ -21,6 +27,6 @@ public class Main extends Game {
 
     @Override
     public void dispose() {
-        batch.dispose();
+//        batch.dispose();
     }
 }
