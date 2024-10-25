b41821eb6e2c73669eed9df5b155b8de769ee46b
Makk18 <147920891+Makk18@users.noreply.github.com>
2024-10-25 11:00:24 +0530

    Update SelectLevelScreen.java

--- SelectLevelScreen.java
+++ SelectLevelScreen.java
@@ -5,10 +5,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SelectLevelScreen implements Screen {
     private Main game;
+    private Stage stage;
     private SpriteBatch batch;
     private Texture backgroundTexture;
 
@@ -16,20 +21,22 @@ public class SelectLevelScreen implements Screen {
 //    private Rectangle selectlevelButtonRectangle;
 
     private Texture level1ButtonTexture;
-    private Rectangle level1ButtonRectangle;
+//    private Rectangle level1ButtonRectangle;
 
     private Texture level2ButtonTexture;
-    private Rectangle level2ButtonRectangle;
+//    private Rectangle level2ButtonRectangle;
 
     private Texture level3ButtonTexture;
-    private Rectangle level3ButtonRectangle;
+//    private Rectangle level3ButtonRectangle;
 
     private Texture backButtonTexture;
-    private Rectangle backButtonRectangle;
+//    private Rectangle backButtonRectangle;
 
     public SelectLevelScreen(Main game) {
         this.game = game;
         batch = new SpriteBatch();
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
         backgroundTexture = new Texture("background2.png");
 
@@ -37,16 +44,63 @@ public class SelectLevelScreen implements Screen {
 //        selectlevelButtonRectangle = new Rectangle(300, 300, 200, 100);
 
         level1ButtonTexture = new Texture("level1.png");
-        level1ButtonRectangle = new Rectangle(300, 300, 200, 50);
+//        level1ButtonRectangle = new Rectangle(300, 300, 200, 50);
+        Drawable level1d = new TextureRegionDrawable(level1ButtonTexture);
+        ImageButton level1Button = new ImageButton(level1d);
+        level1Button.setPosition(250, 370);
+        level1Button.setSize(300, 70);
+        level1Button.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ClickListener(){
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y){
+                game.setScreen(new Level1(game));
+            }
+        });
+
 
         level2ButtonTexture = new Texture("level2.png");
-        level2ButtonRectangle = new Rectangle(300, 250, 200, 50);
+//        level2ButtonRectangle = new Rectangle(300, 250, 200, 50);
+        Drawable level2d = new TextureRegionDrawable(level2ButtonTexture);
+        ImageButton level2Button = new ImageButton(level2d);
+        level2Button.setPosition(250, 270);
+        level2Button.setSize(300, 70);
+        level2Button.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ClickListener(){
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y){
+                game.setScreen(new Level2(game));
+            }
+        });
+
 
         level3ButtonTexture = new Texture("level3.png");
-        level3ButtonRectangle = new Rectangle(300, 200, 200, 50);
+//        level3ButtonRectangle = new Rectangle(300, 200, 200, 50);
+        Drawable level3d = new TextureRegionDrawable(level3ButtonTexture);
+        ImageButton level3Button = new ImageButton(level3d);
+        level3Button.setPosition(250, 170);
+        level3Button.setSize(300, 70);
+        level3Button.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ClickListener(){
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y){
+                game.setScreen(new Level3(game));
+            }
+        });
 
         backButtonTexture = new Texture("backbutton.png");
-        backButtonRectangle = new Rectangle(300, 150, 200, 50);
+//        backButtonRectangle = new Rectangle(300, 150, 200, 50);
+        Drawable backd = new TextureRegionDrawable(backButtonTexture);
+        ImageButton backButton = new ImageButton(backd);
+        backButton.setPosition(250, 70);
+        backButton.setSize(300, 70);
+        backButton.addListener(new com.badlogic.gdx.scenes.scene2d.utils.ClickListener(){
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y){
+                game.setScreen(new MainMenuScreen(game));
+            }
+        });
+
+        stage.addActor(level1Button);
+        stage.addActor(level2Button);
+        stage.addActor(level3Button);
+        stage.addActor(backButton);
     }
 
     @Override
@@ -65,27 +119,29 @@ public class SelectLevelScreen implements Screen {
         batch.begin();
         batch.draw(backgroundTexture,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 //        batch.draw(selectlevelButtonTexture, selectlevelButtonRectangle.x, selectlevelButtonRectangle.y, selectlevelButtonRectangle.width, selectlevelButtonRectangle.height);
-        batch.draw(level1ButtonTexture, level1ButtonRectangle.x, level1ButtonRectangle.y, level1ButtonRectangle.width, level1ButtonRectangle.height);
-        batch.draw(level2ButtonTexture, level2ButtonRectangle.x, level2ButtonRectangle.y, level2ButtonRectangle.width, level2ButtonRectangle.height);
-        batch.draw(level3ButtonTexture, level3ButtonRectangle.x, level3ButtonRectangle.y, level3ButtonRectangle.width, level3ButtonRectangle.height);
-        batch.draw(backButtonTexture, backButtonRectangle.x, backButtonRectangle.y, backButtonRectangle.width, backButtonRectangle.height);
+//        batch.draw(level1ButtonTexture, level1ButtonRectangle.x, level1ButtonRectangle.y, level1ButtonRectangle.width, level1ButtonRectangle.height);
+//        batch.draw(level2ButtonTexture, level2ButtonRectangle.x, level2ButtonRectangle.y, level2ButtonRectangle.width, level2ButtonRectangle.height);
+//        batch.draw(level3ButtonTexture, level3ButtonRectangle.x, level3ButtonRectangle.y, level3ButtonRectangle.width, level3ButtonRectangle.height);
+//        batch.draw(backButtonTexture, backButtonRectangle.x, backButtonRectangle.y, backButtonRectangle.width, backButtonRectangle.height);
 
         batch.end();
 
-        if (Gdx.input.isTouched()) {
-            float touchX = Gdx.input.getX();
-            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
-
-            if (level1ButtonRectangle.contains(touchX, touchY)) {
-                game.setScreen(new Level1(game));
-            } else if (level2ButtonRectangle.contains(touchX, touchY)) {
-                game.setScreen(new Level2(game));
-            } else if (level3ButtonRectangle.contains(touchX, touchY)) {
-                game.setScreen(new Level3(game));
-            } else if (backButtonRectangle.contains(touchX, touchY)) {
-                game.setScreen(new MainMenuScreen(game));
-            }
-        }
+//        if (Gdx.input.isTouched()) {
+//            float touchX = Gdx.input.getX();
+//            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
+//
+//            if (level1ButtonRectangle.contains(touchX, touchY)) {
+//                game.setScreen(new Level1(game));
+//            } else if (level2ButtonRectangle.contains(touchX, touchY)) {
+//                game.setScreen(new Level2(game));
+//            } else if (level3ButtonRectangle.contains(touchX, touchY)) {
+//                game.setScreen(new Level3(game));
+//            } else if (backButtonRectangle.contains(touchX, touchY)) {
+//                game.setScreen(new MainMenuScreen(game));
+//            }
+//        }
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
